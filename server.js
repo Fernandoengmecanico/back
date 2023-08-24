557dff32012f95ffb14e13f60bf38c584849d1c9
Fernando <fernando.ferreira2105@gmail.com>
2023-08-23 21:43:15 -0400

    segundo commit

--- server.js
+++ server.js
@@ -3,12 +3,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 const port = process.env.PORTA;
-/*
-app.use("/", (req, res) => {
-  console.log("entrei");
-  res.sendStatus(404)
-});
-*/
+
 app.use(require("./routes/_routesIndex"));
 
  app.listen(port, () => {
